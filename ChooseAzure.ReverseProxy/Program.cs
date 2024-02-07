4873ba69929734a3ff0004de8705598906872c4b
Kaloyan Drenski <drenski666@gmail.com>
2024-02-07 15:28:25 +0200

    Add project files.

---
+++ ChooseAzure.ReverseProxy/Program.cs
@@ -0,0 +1,10 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("Yarp"));
+
+var app = builder.Build();
+
+app.MapReverseProxy();
+
+app.Run();
